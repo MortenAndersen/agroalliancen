f3feb375e6c8f6a972e97c75db8ebf794bde49ca
Morten Andersen <morten@hjemmesider.dk>
2021-02-24 12:12:10 +0100

    Titel væk

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <main class="main-page">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<?php the_title('<h1>', '</h1>'); ?>
 			<?php the_content(); ?>
 		</article>
 	<?php endwhile; ?>
